914ae687aeca993041e8ebf7bd3f6e288fd3b241
rzisk <rzisk@milestechnologies.com>
2016-06-22 17:28:35 -0400

    Just making sure we don't mess anything up

---
+++ decrypter_poc/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using System.Diagnostics;
+
+namespace decrypter_poc
+{
+    class Program
+    {
+        static int correctTick;
+
+        // PNG magic byte header
+        static byte[] PNG_MAGIC_BYTES = new byte[8] { 137, 80, 78, 71, 13, 10, 26, 10 };
+
+        static bool checkPNG (byte[] decryptedBytes)
+        {
+            byte[] headerArray = new byte[8];
+
+            Array.Copy(decryptedBytes, headerArray, 8);
+
+            if (headerArray.SequenceEqual(PNG_MAGIC_BYTES))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        static public byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes, byte[] saltBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            RijndaelManaged AES = new RijndaelManaged
+            {
+                KeySize = 256,
+                BlockSize = 128,
+                Mode = CipherMode.CBC
+            };
+
+
+            int keyBytes = AES.KeySize / 8;
+            int ivBytes = AES.BlockSize / 8;
+
+
+            try
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(keyBytes);
+                    AES.IV = key.GetBytes(ivBytes);
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.FlushFinalBlock();
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return decryptedBytes;
+        }
+
+
+        public static string GetPass(int x, int seed)
+        {
+            string str = "";
+            Random random = new Random(seed);
+            while (str.Length < x)
+            {
+                char c = (char)random.Next(33, 125);
+                if (char.IsLetterOrDigit(c))
+                    str += c;
+            }
+            return str;
+        }
+
+        static byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        public static byte[] tryDecrypt(string file, int startSeed)
+        {
+            byte[] fileBytes = File.ReadAllBytes(file);
+
+            // Subtract 5 minutes from begining in case boot time is off
+            int startOffset = startSeed - 100;
+
+            // add 5 minutes of miliseconds to end
+            int endSeed = startSeed + 100;
+
+            int attemptTotal = (endSeed - startOffset) * 20;
+            int attemptNumber = 0;
+
+            IEnumerable<int> pwLengths = Enumerable.Range(30, 20);
+
+            var stop = new System.Diagnostics.Stopwatch();
+
+
+            stop.Start();
+
+            int offsetAtDecrypted = 0;
+            var IsDecrypted = false;
+
+            for (int seed = startOffset; seed < endSeed; seed++)
+            {
+                //start password length at 30, loop until 50
+
+                //for (int pwlength = 30; pwlength < 50; pwlength++)
+
+
+                Parallel.ForEach(pwLengths, (pwlength, state) =>
+                {
+                    string pass = GetPass(pwlength, seed);
+                    // orginal ransomware appears to use a hash of the password rather than the real password
+                    byte[] passBytes = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(pass));
+                    byte[] decrypted = AES_Decrypt(fileBytes, passBytes, saltBytes);
+
+
+                    //Debug.WriteLine("Will this allow me in the loops?");
+
+                    if (decrypted != null)
+                    {
+                        if (checkPNG(decrypted))
+                        {
+                            correctTick = seed;
+                            offsetAtDecrypted = seed;
+
+                            state.Break();
+                            IsDecrypted = true;
+
+                        }
+
+                    }
+                    attemptNumber++;
+
+                });
+
+                //Console.Write("\rAttempt {0}/{1}", attemptNumber, attemptTotal);
+                //Console.WriteLine("Exhausted Seed: {0} Length: {1}", seed, pwlength);
+                //});
+
+                if (IsDecrypted)
+                    {
+                        break;
+                    }
+                
+            }
+                stop.Stop();
+                Console.WriteLine("{0} time Elastped, Decrypted seed value {1}", stop.Elapsed, offsetAtDecrypted);
+                Console.ReadLine();
+                return null;
+            
+        }
+        public static void writeDecryptedFile(string cryptedFile, byte[] decryptedBytes)
+        {
+            // create directory
+            DirectoryInfo decryptedDir = Directory.CreateDirectory(Path.GetDirectoryName(cryptedFile) + @"\decrypted");
+            string cleanFile = Path.Combine(decryptedDir.FullName, Path.GetFileName(cryptedFile.Replace(".evil", "")));
+            File.WriteAllBytes(cleanFile ,decryptedBytes);
+        }
+
+        public static int getTicks(DateTime bootDate, string decryptFile)
+        {
+            DateTime lastWrite = File.GetLastWriteTime(decryptFile);
+
+            TimeSpan dateDiff = lastWrite - bootDate;
+
+            var currentTick = Convert.ToInt32(dateDiff.TotalMilliseconds);
+
+            return currentTick;
+        }
+
+        public static DateTime calcActualBoot(string cryptedfile, int foundSeed)
+        {
+            DateTime fileLastWrite = File.GetLastWriteTime(cryptedfile);
+            TimeSpan seedConvert = TimeSpan.FromMilliseconds(foundSeed);
+            DateTime actualBoot = fileLastWrite.Subtract(seedConvert);
+            return actualBoot;
+        }
+
+        static void Main(string[] args)
+        {
+            byte[] decryptedArray;
+            string filePath;
+            DateTime startDate;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Enter full path of encrypted file:");
+                filePath = Console.ReadLine();
+
+                if (filePath == "")
+                {
+                    filePath = @"K:\Temp\Mike Young\googlelogo.png.evil";
+                }
+
+                Console.WriteLine("Enter boot date:");
+
+                var strStartDate = Console.ReadLine();
+
+                if (strStartDate == "")
+                {
+                    startDate = Convert.ToDateTime(@"6/22/2016 8:41:18 AM");
+                } else
+                {
+                    startDate = Convert.ToDateTime(strStartDate);
+                }
+
+                
+            }
+            else
+            {
+                filePath = args[0];
+                startDate = Convert.ToDateTime(args[1]);
+            }
+
+            int startTicks = getTicks(startDate, filePath);
+
+            Console.WriteLine("Attempting decryption...");
+
+            decryptedArray = tryDecrypt(filePath, startTicks);
+
+            if (decryptedArray != null)
+            {
+                Console.WriteLine("\nSuccesfully decrypted file. Writing to disk.");
+                Console.WriteLine("Seed value: {0}", correctTick);
+                DateTime calcDate = calcActualBoot(filePath, correctTick);
+                Console.WriteLine("Machine Boot Time: {0}", calcDate);
+
+                writeDecryptedFile(filePath, decryptedArray);
+            }
+            else 
+            {
+                Console.WriteLine("\nFailed to decrypt file!");
+            }
+        }
+    }
+}
